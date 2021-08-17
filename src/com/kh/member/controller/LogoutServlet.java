2a1c5f962905195db5ec7bf381beb171a718d731
annie <annie@DESKTOP-NAQ6366>
2021-08-17 10:37:01 +0900

    0817 commit

--- src/com/kh/member/controller/LogoutServlet.java
+++ src/com/kh/member/controller/LogoutServlet.java
@@ -26,7 +26,9 @@ public class LogoutServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().invalidate();
 		
+		response.sendRedirect(request.getContextPath());
 	}
 
 	/**
